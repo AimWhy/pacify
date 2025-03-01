4e8bc6d3d206cc3eb5d07da739f589f52cded294
Naveen Kumar <text.nav@gmail.com>
2025-03-01 23:25:11 +0800

    feat: add Monaco options interface and improve error handling in NotifyService

--- src/components/ProxyConfig/PACScriptSettings.svelte
+++ src/components/ProxyConfig/PACScriptSettings.svelte
@@ -7,6 +7,7 @@
   import { scriptTemplates } from '@/constants/templates'
   import Button from '../Button.svelte'
   import { I18nService } from '@/services/i18n/i18nService'
+  import { defaultOptions } from '@/utils/monaco'
 
   export let pacUrl: string = ''
   export let pacMandatory: boolean = false
@@ -27,14 +28,8 @@
     if (!pacUrl) {
       try {
         editor = await Monaco.create(editorContainer, {
+          ...defaultOptions,
           value: editorContent,
-          automaticLayout: true,
-          minimap: { enabled: false },
-          scrollBeyondLastLine: false,
-          fontSize: 14,
-          lineNumbers: 'on',
-          renderLineHighlight: 'all',
-          tabSize: 2,
         })
 
         // Set up content change listener
@@ -101,16 +96,16 @@
         <Button
           minimal
           color="primary"
-          on:click={() => setTemplate(scriptTemplates.pro)}
+          on:click={() => setTemplate(scriptTemplates.advanced)}
         >
-          {I18nService.getMessage('proTemplate')}
+          {I18nService.getMessage('advancedTemplate')}
         </Button>
         <Button
           minimal
           color="primary"
-          on:click={() => setTemplate(scriptTemplates.advanced)}
+          on:click={() => setTemplate(scriptTemplates.pro)}
         >
-          {I18nService.getMessage('advancedTemplate')}
+          {I18nService.getMessage('proTemplate')}
         </Button>
       </FlexGroup>
       <div
